c512fb606e6623f905619726cd7d81c77bf5c00e
erchuan.ma <450595468@qq.com>
2019-09-15 11:07:37 +0800

    Update contract method data

--- common/executive_params.go
+++ common/executive_params.go
@@ -2,7 +2,7 @@ package common
 
 const (
 	CmdIPFSAdd      = "add"
-	CmdIPFSDownload = "download"
+	CmdIPFSDownload = "buy"
 	IPFSFileName    = "name"
 	ApiIPFSAdd      = "http://localhost:5001/api/v0/add"
 	ApiIPFSCAT      = "http://localhost:5001/api/v0/cat?arg="
